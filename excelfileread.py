1110ea7b9e5ce082e1b1b10ab62b0c49aa8568cf
NomadicGeek12 <bashiir_95@live.com>
2020-05-14 16:02:49 +1000

    Read an excel file

---
+++ excelfileread.py
@@ -0,0 +1,22 @@
+import openpyxl
+
+def createFile():
+    global fileName, newFile
+    fileName=input('Enter file name:')
+    newFile=open(fileName,'wb')
+    newFile.close()
+
+def writeFile():
+    global fileName, newFile, userInput
+    fileName=input('Enter file name you want to wrtie to:')
+    newFile=open(fileName,'wb')
+    newFile.write(userInput)
+    newFile.close()
+
+createFile()
+
+workbook= openpyxl.load_workbook('malik.xlsx')
+sheet =workbook.get_sheet_by_name('Sheet2')
+cell =sheet['C2']
+userInput=bytes(cell.value,'utf-8')
+writeFile()
